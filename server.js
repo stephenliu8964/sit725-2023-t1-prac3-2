21e1f890a5df4e7e5bf36f41d0946590c0f092f0
stephenliu8964 <dq89_64@hotmail.com>
2023-07-25 16:31:20 +1000

    minimum requirements + ejs

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
 
+const cardList = [{
+    title: "Kitten 2",
+    path: "images/kitten-2.jpg",
+    link: "About Kitten 2",
+    desciption: "Demo desciption about kitten 2"
+},
+{
+    title: "Kitten 3",
+    path: "images/kitten-3.jpg",
+    link: "About Kitten 3",
+    desciption: "Demo desciption about kitten 3"
+}
+];
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req,res) {
-    res.render('index.html');
+    res.render('pages/index',{cats:cardList})
 });
 
 app.get('/addTwoNumbers', function (req,res) {
